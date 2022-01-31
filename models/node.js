3fdc70a4fd7364ababf9852cd87ffdd42346da9c
Tomas Tamantini <tomasbft@gmail.com>
2022-01-31 20:53:55 -0300

    Set label from id if undefined

--- models/node.js
+++ models/node.js
@@ -3,10 +3,14 @@ class Node {
   #label
   constructor(id, label) {
     this.#id = id
-    this.#label = label ? label : ""
+    this.#label = typeof label === "string" ? label : String(this.#id)
   }
 
   get id() {
     return this.#id
+  }
+
+  get label() {
+    return this.#label
   }
 }
