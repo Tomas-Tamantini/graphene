ab7d6d45c1e6ab9c396235eb1f35af131ea0e2a2
Tomas Tamantini <tomasbft@gmail.com>
2022-01-31 19:57:03 -0300

    Check if edge connects same pair of nodes

--- models/edge.js
+++ models/edge.js
@@ -7,4 +7,28 @@ class Edge {
     this.#source = sourceNode
     this.#target = targetNode
   }
+
+  get sourceNode() {
+    return this.#source
+  }
+
+  set sourceNode(value) {
+    this.#source = value
+  }
+
+  get targetNode() {
+    return this.#target
+  }
+
+  set targetNode(value) {
+    this.#target = value
+  }
+
+  connectsSameNodes(otherEdge) {
+    if (this.#source.id == otherEdge.#source.id)
+      return this.#target.id == otherEdge.#target.id
+    if (this.#source.id == otherEdge.#target.id)
+      return this.#target.id == otherEdge.#source.id
+    return false
+  }
 }
--- tests/test_edge.js
+++ tests/test_edge.js
@@ -5,3 +5,18 @@ it("should connect different nodes", function () {
     e = new Edge(n1, n2)
   })
 })
+
+it("should recognize another label which connects to same endpoints", function () {
+  n1 = { id: 1 }
+  n2 = { id: 2 }
+  n3 = { id: 3 }
+  e1 = new Edge(n1, n2)
+  e2 = new Edge(n1, n2)
+  e3 = new Edge(n2, n1)
+  e4 = new Edge(n2, n3)
+  assert(
+    e1.connectsSameNodes(e2) &&
+      e1.connectsSameNodes(e3) &&
+      !e1.connectsSameNodes(e4)
+  )
+})
