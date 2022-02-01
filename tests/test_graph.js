b52abbdbc7220514300ed089882dabe4cb5671fd
Tomas Tamantini <tomasbft@gmail.com>
2022-02-01 10:03:29 -0300

    Add missing "let"

--- tests/test_graph.js
+++ tests/test_graph.js
@@ -1,30 +1,30 @@
 it("should reject duplicate nodes", function () {
-  n1 = { id: 1 }
-  n2 = { id: 1 }
-  nodes = [n1, n2]
+  let n1 = { id: 1 }
+  let n2 = { id: 1 }
+  let nodes = [n1, n2]
   assertRaisesError(() => {
-    g = new Graph(nodes)
+    let g = new Graph(nodes)
   })
 })
 
 it("should reject duplicate edges", function () {
-  n1 = { id: 1 }
-  n2 = { id: 2 }
-  e1 = new Edge(n1, n2)
-  e2 = new Edge(n2, n1)
-  nodes = null
-  edges = [e1, e2]
+  let n1 = { id: 1 }
+  let n2 = { id: 2 }
+  let e1 = new Edge(n1, n2)
+  let e2 = new Edge(n2, n1)
+  let nodes = null
+  let edges = [e1, e2]
   assertRaisesError(() => {
-    g = new Graph(nodes, edges)
+    let g = new Graph(nodes, edges)
   })
 })
 
 it("should account for all nodes", function () {
-  n1 = { id: 1 }
-  n2 = { id: 2 }
-  e = new Edge(n1, n2)
-  nodes = [n1]
-  edges = [e1] // n2 should be accounted for because it is in e1
-  g = new Graph(nodes, edges)
+  let n1 = { id: 1 }
+  let n2 = { id: 2 }
+  let e = new Edge(n1, n2)
+  let nodes = [n1]
+  let edges = [e1] // n2 should be accounted for because it is in e1
+  let g = new Graph(nodes, edges)
   assert(g.numEdges === 1 && g.numNodes === 2)
 })
--- tests/test_parse_graph.js
+++ tests/test_parse_graph.js
@@ -1,24 +1,28 @@
 it("should account for all nodes", function () {
-  inputObj = {
+  let inputObj = {
     nodes: [{ id: 1 }, { id: 2 }],
   }
-  g = parseGraph(inputObj)
+  let g = parseGraph(inputObj)
   assert(g.numEdges === 0 && g.numNodes === 2)
 })
 
 it("should account for all edges", function () {
-  inputObj = {
+  let inputObj = {
     nodes: [{ id: "A" }], // B is implied from edges
     edges: [{ sourceId: "A", targetId: "B" }],
   }
-  g = parseGraph(inputObj)
+  let g = parseGraph(inputObj)
   assert(g.numEdges === 1 && g.numNodes === 2)
 })
 
-it("should give node random position when none is provided", () => {
-  n1 = { id: "A" }
-  n2 = { id: "B", x: 10, y: 20 }
-  nodes = parseNodes([n1, n2])
-  assert(typeof nodes[0].x === "number" && typeof nodes[0].y === "number")
-  assert(nodes[1].x === 10 && nodes[1].y === 20)
+it("should load even when nodes key is ommited", () => {
+  let inputObj = {
+    edges: [
+      { sourceId: "A", targetId: "B" },
+      { sourceId: "A", targetId: "C" },
+      { sourceId: "A", targetId: "D" },
+    ],
+  }
+  let g = parseGraph(inputObj)
+  assert(g.numEdges === 3 && g.numNodes === 4)
 })
