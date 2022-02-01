6ee3e0b96e3cacdf659856d94535fd4eba2db024
Tomas Tamantini <tomasbft@gmail.com>
2022-02-01 10:19:08 -0300

    Draw nodes and edges

--- draw_methods/draw_graph.js
+++ draw_methods/draw_graph.js
@@ -1,3 +1,34 @@
 function drawGraph(graph) {
-  ellipse(100, 100, 100)
+  drawEdges(graph.edges)
+  drawNodes(graph.nodes)
+}
+
+function drawNodes(nodes) {
+  nodes.forEach((node) => {
+    drawNode(node)
+  })
+}
+
+function drawEdges(edges) {
+  edges.forEach((edge) => {
+    drawEdge(edge)
+  })
+}
+
+function drawEdge(edge) {
+  let p1 = getNodePosOnCanvas(edge.sourceNode)
+  let p2 = getNodePosOnCanvas(edge.targetNode)
+  line(p1.x, p1.y, p2.x, p2.y)
+}
+
+function drawNode(node) {
+  let { x, y } = getNodePosOnCanvas(node)
+  circle(x, y, 10)
+}
+
+function getNodePosOnCanvas(node) {
+  let posTranslate = width / 100
+  let x = node.x * posTranslate
+  let y = node.y * posTranslate
+  return { x, y }
 }
